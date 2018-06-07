0968f97a97345a6c66d2127d24720c5fec526d05
Chacko <chackop@gmail.com>
2018-06-07 16:31:15 +0100

    added header main page components

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './main-page.css';
+import Header from "./header";
+import FeaturedHouse from './feature-house';
 
 class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    this.fetchHouses();
+  }
+
+  fetchHouses = () => {
+    fetch('/houses.json')
+      .then(rsp => rsp.json())
+      .then(allHouses => {
+        this.allHouses = allHouses;
+        this.determineFeaturedHouse();
+        this.determineUniqueCountries();
+      })
+  }
+
+  determineFeaturedHouse = () => {
+    if (this.allHouses) {
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse = this.allHouses[randomIndex];
+      this.setState({ featuredHouse });
+    }
+  }
+
+  determineUniqueCountries = () => {
+    const countries = this.allHouses ? Array.from(new Set(this.allHouses.map(h => h.country))) : [];
+    countries.unshift(null);
+    this.setState({ countries });
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Header subtitle="This a show home site" />
+        <FeaturedHouse house={this.state.featuredHouse} />
       </div>
     );
   }
